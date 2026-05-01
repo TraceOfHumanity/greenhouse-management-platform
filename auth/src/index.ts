bcb97a39add556f5f1971c8b97b94003d6a7335d
TraceOfHumanity <azalexxx02@gmail.com>
2026-05-02 00:23:02 +0300

    Add Kubernetes configuration for auth module, including deployment, service, and ingress resources. Update Express server to handle user ID requests in index.ts. Introduce skaffold.yaml for build and deployment management.

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,15 @@ import express from "express";
 
 const app = express();
 
+// app.get('/api/users/currentuser', (req, res) => {
+//     res.json({ user: 'Alex' });
+// });
+
+app.get('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    res.json({ user: id });
+});
+
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
